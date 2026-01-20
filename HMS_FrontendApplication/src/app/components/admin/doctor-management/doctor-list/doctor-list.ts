a06d047be976866fccc41c86773adc744b4a5b33
AkashGaygol99 <akashgaygol560@gmail.com>
2026-01-20 13:28:43 +0530

    Pagination
    Added Pagination for doctor-list from Admin-Dashboard. It has limit of 10 records per page after that it redirects to next page

--- HMS_FrontendApplication/src/app/components/admin/doctor-management/doctor-list/doctor-list.ts
+++ HMS_FrontendApplication/src/app/components/admin/doctor-management/doctor-list/doctor-list.ts
@@ -1,8 +1,6 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DoctorService } from '../../../../services/doctor.service';
-import { finalize } from 'rxjs';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-doctor-list',
@@ -13,48 +11,79 @@ import { CommonModule } from '@angular/common';
 export class DoctorList implements OnInit {
 
   doctors: any[] = [];
-  loading=false;
+  paginatedDoctors: any[] = [];
+
+  loading = false;
+
+  // Pagination variables
+  currentPage = 1;
+  itemsPerPage = 10;
+  totalPages = 0;
 
   constructor(
     private doctorService: DoctorService,
     private router: Router,
-     private cdr: ChangeDetectorRef
+    private cdr: ChangeDetectorRef
   ) {}
 
   ngOnInit() {
     const token = localStorage.getItem('token');
+    if (!token) return;
 
-  if (!token) return;
-   setTimeout(() => {
-    this.loadDoctors();
-  });
+    setTimeout(() => {
+      this.loadDoctors();
+    });
   }
 
-  loadDoctors():void {
-    this.loading=true;
+  loadDoctors(): void {
+    this.loading = true;
+
     this.doctorService.getAllDoctors().subscribe({
       next: (data) => {
-        console.log('Doctors API Response:',data);
         this.doctors = data;
+        this.totalPages = Math.ceil(this.doctors.length / this.itemsPerPage);
+
+        this.setPage(1); // default load page 1
         this.cdr.detectChanges();
-        console.log('Doctors loaded on refresh ',this.doctors.length);
-        this.loading=false;
-        
+
+        this.loading = false;
       },
       error: (err) => {
         console.error("Error loading doctors: ", err);
-
-        this.loading=false;
+        this.loading = false;
       }
     });
   }
-  
 
-  goToAddDoctor():void {
+  // Pagination logic
+  setPage(page: number) {
+    if (page < 1 || page > this.totalPages) return;
+
+    this.currentPage = page;
+
+    const start = (page - 1) * this.itemsPerPage;
+    const end = start + this.itemsPerPage;
+
+    this.paginatedDoctors = this.doctors.slice(start, end);
+  }
+
+  nextPage() {
+    if (this.currentPage < this.totalPages) {
+      this.setPage(this.currentPage + 1);
+    }
+  }
+
+  previousPage() {
+    if (this.currentPage > 1) {
+      this.setPage(this.currentPage - 1);
+    }
+  }
+
+  goToAddDoctor(): void {
     this.router.navigate(['/admin/doctor-management/doctor-add']);
   }
 
-  editDoctor(id: number):void {
+  editDoctor(id: number): void {
     this.router.navigate(['/admin/doctor-management/doctor-edit', id]);
   }
 
